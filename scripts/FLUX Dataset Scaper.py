7cd605c48d2fb1fb6de235fba4c6ab721e582460
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2019-09-29 22:28:29 -0500

    Normalize Standard Deviation Relative to Dataset Standard Deviations

--- scripts/FLUX Dataset Scaper.py
+++ scripts/FLUX Dataset Scaper.py
@@ -10,10 +10,9 @@
 #   1. Planetary (0,1)
 #   2. Average FLUX
 #   3. Median FLUX
-#   4. Mode FLUX
-#   5. Max FLUX
-#   6. Min FLUX
-#   7. Std. Dev. FLUX
+#   4. Max FLUX
+#   5. Min FLUX
+#   6. Std. Dev. FLUX
 #
 # Raw Source: https://www.kaggle.com/keplersmachines/kepler-labelled-time-series-data
 #
@@ -25,7 +24,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Declare Constants
-SAMPLE_ROWS = 5087 # Max = Total (Total Number of Rows: 5087)
+DECIMAL_ACCURACY = 7
+SAMPLE_ROWS = 570 # Max = Total (Total Number of Rows: 5087)
 SAMPLE_COLUMNS = 3197 # Max = Total - 1 (Total Number of Columns: 3198)
 
 # Declare Variables
@@ -33,7 +33,7 @@ system_metadata = []
 
 # Retrieve Dataset
 dataset = np.genfromtxt(
-    '/kaggle/input/kepler-labelled-time-series-data/exoTrain.csv',
+    '/kaggle/input/kepler-labelled-time-series-data/exoTest.csv',
     delimiter=',',
     usecols=(range(SAMPLE_COLUMNS + 1))
 )
@@ -44,33 +44,48 @@ for current_system in range(1, SAMPLE_ROWS + 1):
     # Extract System Data
     star_system_flux_values = dataset[current_system,1:] # Get Flux Values for System
 
-    # Compute System Metadata (Planetary, Mean, Median, Mode, Max, Min, Standard Deviation)
+    # Compute System Metadata (Planetary, Mean, Median, Max, Min, Standard Deviation)
     star_system_metadata = [
-        dataset[current_system,0] - 1,                          # Planetary (0,1)
-        round(np.average(star_system_flux_values), 2),          # Average FLUX
-        round(np.median(star_system_flux_values), 2),           # Median FLUX
-        round(np.amax(star_system_flux_values), 2),             # Max FLUX
-        round(np.amin(star_system_flux_values), 2),             # Min FLUX    DUP??
-        round(np.std(star_system_flux_values), 2)               # Std. Dev. FLUX
+        dataset[current_system,0] - 1,                                         # Planetary (0,1)
+        round(np.average(star_system_flux_values), DECIMAL_ACCURACY),          # Average FLUX
+        round(np.median(star_system_flux_values), DECIMAL_ACCURACY),           # Median FLUX
+        round(np.amax(star_system_flux_values), DECIMAL_ACCURACY),             # Max FLUX
+        round(np.amin(star_system_flux_values), DECIMAL_ACCURACY),             # Min FLUX    DUP??
+        round(np.std(star_system_flux_values), DECIMAL_ACCURACY)               # Std. Dev. FLUX
     ]
     
-    # Normalize Metadata
+    # Normalize Mean, Median, Max, & Min Relative to System Values
     normalized_data = [star_system_metadata[0]]
     min = star_system_metadata[1]
     max = star_system_metadata[1]
-    for record in range(1, len(star_system_metadata)):
+    for record in range(1, len(star_system_metadata) - 1):
         if star_system_metadata[record] < min:
             min = star_system_metadata[record]
         if star_system_metadata[record] > max:
             max = star_system_metadata[record]
-    for record in range(1, len(star_system_metadata)):
+    for record in range(1, len(star_system_metadata) - 1):
         if max - min != 0:
-            normalized_data.append(round((star_system_metadata[record] - min)/(max - min), 4))
+            normalized_data.append(round((star_system_metadata[record] - min)/(max - min), DECIMAL_ACCURACY))
         else:
             normalized_data.append(0.0)
     
     # Save Metadata
     system_metadata.append(normalized_data)
+
+    
+# Normalize Standard Deviation Relative to Dataset Values
+min = dataset[1][5]
+max = dataset[1][5]
+for current_system in range(1, SAMPLE_ROWS + 1):
+    if dataset[current_system][5] < min:
+        min = dataset[current_system][5]
+    if dataset[current_system][5] > max:
+        max = dataset[current_system][5]
+for current_system in range(1, SAMPLE_ROWS + 1):
+    if max - min != 0:
+        system_metadata[current_system - 1].append(round((dataset[current_system][5] - min)/(max - min), DECIMAL_ACCURACY))
+    else:
+        system_metadata[current_system - 1].append(0.0)
 
 # Print Records
 for system in range(0, len(system_metadata)):
