46ae7179a06c15270b010adfe2ae38825bdbc232
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2019-09-29 23:06:23 -0500

    Update Visualization Script

--- scripts/AI Project Proposal.py
+++ scripts/AI Project Proposal.py
@@ -1,36 +1,43 @@
-# AI Project Proposal - Dataset Analysis
+# AI Project Proposal - Normalized Dataset Analysis
 # Christopher Cruzen
 # 09.29.2019
 #
-# Note: This code was run through kaggle's provided notebook editor,
-#       which somewhat simplified the data import process. Link:
-#       https://www.kaggle.com/cacruzen/ai-project-proposal/edit
+# This program converts normalized star system FLUX data
+# into a number of useful visualizations for my machine
+# learning project proposal. 
 
-import numpy as np # linear algebra
+import numpy as np
 import matplotlib.pyplot as plt
-
-# Declare Constants
-SAMPLE_ROW = 25 # Row 0 Contains Headers
-SAMPLE_COLUMNS = 40
 
 # Retrieve Dataset
 dataset = np.genfromtxt(
-    '/kaggle/input/kepler-labelled-time-series-data/exoTrain.csv',
-    delimiter=',',
-    usecols=(range(SAMPLE_COLUMNS + 1)) # Columns 1-11, Total Number of Columns: 3198
+    'https://raw.githubusercontent.com/Xephorium/ExoplanetRecognition/master/datasets/Train.csv',
+    delimiter=","
 )
 
-# Print Dataset Dimensions
-print(dataset.shape)
-
-# Extract Column Data
-star_system_flux_values = dataset[SAMPLE_ROW,1:] # Get Flux Values for System
-star_system_flux_items = np.arange(len(star_system_flux_values))
-
-# Plot Column
-plt.bar(star_system_flux_items, star_system_flux_values, align='center', alpha=.5)
-plt.xticks(np.arange(0, SAMPLE_COLUMNS, 2))
-plt.title('System ' + str(SAMPLE_ROW) + ' Preview')
-plt.ylabel('FLUX Value')
-plt.xlabel('Record')
-plt.show()
+# Plot Aggregate Column Data
+for column in range(1, 6):
+  
+  # Get Column
+  values = dataset[0:,column]
+  
+  # Plot Histogram
+  plt.hist(values, 30, alpha=.5)
+  plt.ylabel('Occurrences')
+  
+  # Set Title
+  title = ''
+  if (column == 1):
+    title = 'Normalized Mean FLUX'
+  elif (column == 2):
+    title = 'Normalized Median FLUX'
+  elif (column == 3):
+    title = 'Normalized Max FLUX'
+  elif (column == 4):
+    title = 'Normalized Min FLUX'
+  else:
+    title = 'Normalized Std. Dev. of FLUX'
+  plt.xlabel(title)
+  
+  # Display Graph
+  plt.show()
